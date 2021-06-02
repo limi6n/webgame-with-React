24127818034ebdbb9d4019e32592be6201a3b2a9
anhl <anhl@iteyes.co.kr>
2021-06-02 17:45:13 +0900

    webpack init

---
+++ react-router/client.jsx
@@ -0,0 +1,6 @@
+const React = require("react");
+const ReactDom = require("react-dom");
+
+const WordRelay = require("../wordrelay/wordRelay");
+
+ReactDom.render(<WordRelay />, document.querySelector("#root"));
